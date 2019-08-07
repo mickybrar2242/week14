7cf8c96accc47ed38ed71944b8b836ca7d20682a
Amitdeep Brar <754202@student.tor.loc>
2019-08-07 15:38:04 -0400

    fourth commit

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const http = require('http');
 
 const site = http.createServer(function(req,res){
     console.log('hello world')
+    res.setHeader('content-type','text/html');
+    console.log(req.rawHeader);
+    
+    res.end("what should i do here");
 })
 site.listen(3000);
