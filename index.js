1c9a6841c0c60639748146db3ccd808fbcef10f1
Amitdeep Brar <754202@student.tor.loc>
2019-08-07 16:18:09 -0400

    sixth commit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const http = require('http'); 
-
-const site = http.createServer(function(req,res){
-    console.log('hello world')
-    res.setHeader('content-type','text/html');
-    console.log(req.rawHeader);
-    
-    res.end("<h1>what should i do here</h1>");
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+    res.send('hello world')
 })
-site.listen(3000);
+app.listen(3000);
