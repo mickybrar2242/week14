6f4560fd4bf59405cb21324d85bcba567e87bea0
Amitdeep Brar <754202@student.tor.loc>
2019-08-07 17:00:07 -0400

    seven commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
-var app = express();
-app.get('/',function(req,res){
-    res.send('hello world')
+var app = express()
+
+app.get('/', function(req, res) {
+res.send('
+
+Hello Word!
+')
 })
-app.listen(3000);
+
+app.get('/', function(req, res) {
+res.send('WHATS there')
+})
+
+app.listen(3000)
